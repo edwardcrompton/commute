aa3677c85a001f686eaae74154fba7f155633537
Edward Crompton <edward.crompton@gmail.com>
2016-02-29 11:43:45 +0000

    Station class with doctrine generated getters and setters

---
+++ README.md
@@ -0,0 +1,4 @@
+commute
+=======
+
+A Symfony project created on February 29, 2016, 11:08 am.
---
+++ src/AppBundle/Entity/Station.php
@@ -0,0 +1,119 @@
+<?php
+
+// src/AppBundle/Entity/Station.php
+namespace AppBundle\Entity;
+
+class Station
+{
+    protected $name;
+    protected $code;
+    protected $lat;
+    protected $long;
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Station
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Station
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set lat
+     *
+     * @param string $lat
+     * @return Station
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * Get lat
+     *
+     * @return string 
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * Set long
+     *
+     * @param string $long
+     * @return Station
+     */
+    public function setLong($long)
+    {
+        $this->long = $long;
+
+        return $this;
+    }
+
+    /**
+     * Get long
+     *
+     * @return string 
+     */
+    public function getLong()
+    {
+        return $this->long;
+    }
+}
