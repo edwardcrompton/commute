b4d68f2d209ec97c5eca009dbecf609d978523c1
Edward Crompton <edward.crompton@cameronandwilding.com>
2016-03-01 16:52:49 +0000

    Correcting whitespace

--- src/AppBundle/Manager/StationsFeedManager.php
+++ src/AppBundle/Manager/StationsFeedManager.php
@@ -16,19 +16,19 @@ use \Curl\Curl;
  */
 class StationsFeedManager {
   
-  const TRANSPORT_API_URL = 'http://transportapi.com/v3/uk/train/stations/bbox.json';
-  
-  /**
-   * 
-   */
-  public function __construct() {
-    $this->app_id = '98b1d17e';
-    $this->app_key = 'f4f457e81bb4f207fdfe0e418d5eec6f';
-    $this->maxlat = '53.6';
-    $this->maxlon = '-1.5';
-    $this->minlat = '51.0';
-    $this->minlon = '-5.5';
-  }
+    const TRANSPORT_API_URL = 'http://transportapi.com/v3/uk/train/stations/bbox.json';
+
+    /**
+     *
+     */
+    public function __construct() {
+        $this->app_id = '98b1d17e';
+        $this->app_key = 'f4f457e81bb4f207fdfe0e418d5eec6f';
+        $this->maxlat = '53.6';
+        $this->maxlon = '-1.5';
+        $this->minlat = '51.0';
+        $this->minlon = '-5.5';
+    }
   
   /**
    * Issues a curl request to the api for stations we require.
@@ -36,14 +36,14 @@ class StationsFeedManager {
    * @return null|curl response
    */
   public function fetchStations($page = 1) {
-    $requestVars = array(
-      'app_id' => $this->app_id,
-      'app_key' => $this->app_key,
-      'maxlat' => $this->maxlat,
-      'maxlon' => $this->maxlon,
-      'minlat' => $this->minlat,
-      'minlon' => $this->minlon,
-      'page' => $page,
+      $requestVars = array(
+          'app_id' => $this->app_id,
+          'app_key' => $this->app_key,
+          'maxlat' => $this->maxlat,
+          'maxlon' => $this->maxlon,
+          'minlat' => $this->minlat,
+          'minlon' => $this->minlon,
+          'page' => $page,
     );
 
     $curl = new Curl();
